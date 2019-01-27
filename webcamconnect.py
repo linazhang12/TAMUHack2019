7de00043dbb8b8cbc597a420b9f0027be023e410
unknown <linazhang13579@gmail.com>
2019-01-26 18:03:11 -0600

    webcam works w a few errors

---
+++ webcamconnect.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
+
+
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for  (x, y, w, h) in faces:
+        cv.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+
+
+
+while(True):
+    # Capture frame-by-frame
+    _, frame = cap.read()
+
+    # Our operations on the frame come here
+    if _ is True:
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    else:
+        continue
+    # Display the resulting frame
+    canvas = detect(gray, frame)
+    cv.imshow("Eye tracker", frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
